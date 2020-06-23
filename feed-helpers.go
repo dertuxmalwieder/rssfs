6bb4f1785115218dd90819a177a489f02eb2dfa7
i1i1 <vanyarybin1@live.ru>
2020-06-23 23:59:09 +0300

    Fix filenames. If names collide, they will be numbered.

--- feed-helpers.go
+++ feed-helpers.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"time"
-	
+
 	"github.com/mmcdole/gofeed"
 	"jaytaylor.com/html2text"
 )
+
+type ByTitle []*gofeed.Item
+
+func (a ByTitle) Len() int      { return len(a) }
+func (a ByTitle) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByTitle) Less(i, j int) bool {
+	return fileNameClean(a[i].Title) < fileNameClean(a[j].Title)
+}
+
+func getItemTimestamp(item *gofeed.Item) time.Time {
+	if item.UpdatedParsed != nil {
+		return *(item.UpdatedParsed)
+	}
+	if item.PublishedParsed != nil {
+		return *(item.PublishedParsed)
+	}
+	return time.Now()
+}
 
 func UpdateSingleFeed(feed *Feed, nodeCount uint64) ([]*IndexedFile, uint64, *gofeed.Feed) {
 	// Updates a single feed. Returns the new list of IndexedFiles,
@@ -15,26 +34,37 @@ func UpdateSingleFeed(feed *Feed, nodeCount uint64) ([]*IndexedFile, uint64, *go
 	feeddata, _ := fp.ParseURL(feed.URL)
 	feedFiles := make([]*IndexedFile, 0)
 
+	sort.Sort(ByTitle(feeddata.Items))
+
+	fname, prev_fname := "", ""
+	// File collision counter
+	col_cnt := 0
+
 	// Add files to the feeds:
 	for _, item := range feeddata.Items {
-		var itemTimestamp time.Time
-		
-		if (item.UpdatedParsed != nil) {
-			itemTimestamp = *(item.UpdatedParsed)
+		itemTimestamp := getItemTimestamp(item)
+
+		// Checking collision
+		fname = fileNameClean(item.Title)
+		if fname == prev_fname {
+			col_cnt += 1
 		} else {
-			if (item.PublishedParsed != nil) {
-				itemTimestamp = *(item.PublishedParsed)
-			} else {
-				itemTimestamp = time.Now()
-			}
+			col_cnt = 0
 		}
+		prev_fname = fname
 
 		extension, content := GenerateOutputData(feed, item)
+		if col_cnt > 0 {
+			fname = fmt.Sprintf("%s [%d].%s", fname, col_cnt, extension)
+		} else {
+			fname = fmt.Sprintf("%s.%s", fname, extension)
+		}
+
 		feedFiles = append(feedFiles, &IndexedFile{
-			Filename:    fmt.Sprintf("%s.%s", fileNameClean(item.Title), extension),
-			Timestamp:   itemTimestamp,
-			Inode:       nodeCount,
-			Data:        []byte(content),
+			Filename:  fname,
+			Timestamp: itemTimestamp,
+			Inode:     nodeCount,
+			Data:      []byte(content),
 		})
 
 		nodeCount++
@@ -43,7 +73,7 @@ func UpdateSingleFeed(feed *Feed, nodeCount uint64) ([]*IndexedFile, uint64, *go
 	return feedFiles, nodeCount, feeddata
 }
 
-func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
+func PopulateFeedTree(cfg RssfsConfig) map[string][]*IndexedFile {
 	// Generates a file system, returns a tree of folders and files.
 	// This updates each feed in the tree, it can be a relatively
 	// slow operation on more than just a handful of feeds...
@@ -51,10 +81,10 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 	nodeCount := uint64(1001)
 	feedFiles := make([]*IndexedFile, 0)
 	var feeddata *gofeed.Feed
-	
+
 	rootItems := make([]*IndexedFile, 0)
 	fp := gofeed.NewParser()
-	
+
 	for _, category := range cfg.Categories {
 		// Add each category as a subdirectory.
 		catsFeeds := make([]*IndexedFile, 0)
@@ -66,10 +96,10 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 			var nodeTimestamp time.Time
 
 			// Note: Certain blogs won't send a valid time stamp. Urgh.
-			if (feeddata.UpdatedParsed != nil) {
+			if feeddata.UpdatedParsed != nil {
 				nodeTimestamp = *(feeddata.UpdatedParsed)
 			} else {
-				if (feeddata.PublishedParsed != nil) {
+				if feeddata.PublishedParsed != nil {
 					nodeTimestamp = *(feeddata.PublishedParsed)
 				} else {
 					nodeTimestamp = time.Now()
@@ -86,11 +116,11 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 
 			nodeCount += 100
 
-			retval["/" + fileNameClean(category.Name) + "/" + fileNameClean(feeddata.Title)] = feedFiles
+			retval["/"+fileNameClean(category.Name)+"/"+fileNameClean(feeddata.Title)] = feedFiles
 		}
 
-		retval["/" + fileNameClean(category.Name)] = catsFeeds
-		
+		retval["/"+fileNameClean(category.Name)] = catsFeeds
+
 		// Finally, append this category to our root structure:
 		rootItems = append(rootItems, &IndexedFile{
 			Filename:    fileNameClean(category.Name),
@@ -98,7 +128,7 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 			Timestamp:   time.Now(),
 			Inode:       nodeCount,
 		})
-		
+
 		nodeCount++
 	}
 
@@ -109,10 +139,10 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 		var nodeTimestamp time.Time
 
 		// Note: Certain blogs won't send a valid time stamp. Urgh.
-		if (feeddata.UpdatedParsed != nil) {
+		if feeddata.UpdatedParsed != nil {
 			nodeTimestamp = *(feeddata.UpdatedParsed)
 		} else {
-			if (feeddata.PublishedParsed != nil) {
+			if feeddata.PublishedParsed != nil {
 				nodeTimestamp = *(feeddata.PublishedParsed)
 			} else {
 				nodeTimestamp = time.Now()
@@ -126,14 +156,14 @@ func PopulateFeedTree(cfg RssfsConfig) (map[string][]*IndexedFile) {
 			Inode:       nodeCount,
 			Feed:        feed,
 		})
-		
+
 		nodeCount += 100
-		
+
 		// Add files to the feeds:
 		feedFiles := make([]*IndexedFile, 0)
 		feedFiles, nodeCount, feeddata = UpdateSingleFeed(feed, nodeCount)
 
-		retval["/" + fileNameClean(feeddata.Title)] = feedFiles
+		retval["/"+fileNameClean(feeddata.Title)] = feedFiles
 	}
 
 	// Finalize the tree:
@@ -146,22 +176,22 @@ func GenerateOutputData(feedopts *Feed, item *gofeed.Item) (ext string, content 
 	// Generates the output file (extension and content) for an item.
 	// Takes the feed's options as the first parameter to determine
 	// whether to use plain text and to add the link.
-	if (feedopts.PlainText) {
+	if feedopts.PlainText {
 		// Parse into plain text:
-		outContent, _ := html2text.FromString(item.Content, html2text.Options{ PrettyTables: true, OmitLinks: false })
+		outContent, _ := html2text.FromString(item.Content, html2text.Options{PrettyTables: true, OmitLinks: false})
 
 		// Prepend the title and the link (if wanted):
 		link := ""
-		if (feedopts.ShowLink && item.Link != "") {
+		if feedopts.ShowLink && item.Link != "" {
 			link = fmt.Sprintf("%s%s", LineBreak, item.Link)
 		}
 		content = fmt.Sprintf("%s%s%s%s%s", item.Title, link, LineBreak, LineBreak, outContent)
 		ext = "txt"
 	} else {
 		outTitle := ""
-		
+
 		// Prepend the title and link (if wanted):
-		if (feedopts.ShowLink && item.Link != "") {
+		if feedopts.ShowLink && item.Link != "" {
 			outTitle = fmt.Sprintf("<h1><a href=\"%s\">%s</a></h1>", item.Link, item.Title)
 		} else {
 			outTitle = fmt.Sprintf("<h1>%s</h1>", item.Title)
