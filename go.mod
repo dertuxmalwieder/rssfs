875f3ce443b6fce212745a1e91b3c165d349c6fd
Cthulhux <Cthulhux@noemail.net>
2020-09-20 16:42:44 +0000

    Fallback when XDG_CONFIG_HOME is empty.
    FossilOrigin-Name: f96596d711bf0a22fad9a502914c8f5159515a6446cbb8b58bc1880f97123b65

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/hanwen/go-fuse/v2 v2.0.3
 	github.com/hashicorp/hcl/v2 v2.6.0
 	github.com/kyokomi/emoji v2.2.4+incompatible
-	github.com/mmcdole/gofeed v1.0.0
+	github.com/mmcdole/gofeed v1.1.0
 	github.com/olekukonko/tablewriter v0.0.4 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
--- os-unixoid.go
+++ os-unixoid.go
@@ -21,11 +21,19 @@ func ConfigFilePath() string {
 	// Returns the path to our configuration file, whether it exists or not.
 	// On non-macOS systems, this should be $XDG_CONFIG_HOME/rssfs.hcl, on
 	// macOS, it is ${HOME}/Library/Application Support/rssfs.hcl.
+	// If $XDG_CONFIG_HOME is not defined, non-macOS systems will fall back
+	// to $HOME/.config.
 	path := ""
 	if runtime.GOOS == "darwin" {
 		path = fmt.Sprintf("%s/Library/Application Support", os.Getenv("HOME"))
 	} else {
-		path = os.Getenv("XDG_CONFIG_HOME")
+		_, xdgDefined := os.LookupEnv("XDG_CONFIG_HOME")
+		if !xdgDefined {
+			// Fallback:
+			path = fmt.Sprintf("%s/.config", os.Getenv("HOME"))
+		} else {
+			path = os.Getenv("XDG_CONFIG_HOME")
+		}
 	}
 	return fmt.Sprintf("%s/rssfs.hcl", path)
 }
